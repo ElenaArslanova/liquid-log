78e48056d65dd00f494b0940c98d5e27f298961e
ElenaArslanova <contilen@gmail.com>
2018-12-03 16:29:13 +0500

    issue 10

--- src/main/java/ru/naumen/sd40/log/parser/LogParser.java
+++ src/main/java/ru/naumen/sd40/log/parser/LogParser.java
@@ -5,16 +5,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.security.InvalidParameterException;
 import java.text.ParseException;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.naumen.perfhouse.uploaders.*;
-import ru.naumen.sd40.log.parser.gc.GCDataParser;
-import ru.naumen.sd40.log.parser.gc.GCTimeParserFactory;
-import ru.naumen.sd40.log.parser.sdng.SdngDataParser;
-import ru.naumen.sd40.log.parser.sdng.SdngTimeParserFactory;
-import ru.naumen.sd40.log.parser.top.TopDataParser;
-import ru.naumen.sd40.log.parser.top.TopTimeParserFactory;
+import ru.naumen.sd40.log.parser.mode.Mode;
 
 /**
  * Created by doki on 22.10.16.
@@ -22,39 +18,11 @@ import ru.naumen.sd40.log.parser.top.TopTimeParserFactory;
 @Component
 public class LogParser
 {
-    private SdngDataParser sdngDataParser;
-    private GCDataParser gcDataParser;
-    private TopDataParser topDataParser;
-
-    private SdngDataSetUploaderFactory sdngDataSetUploaderFactory;
-    private GCDataSetUploaderFactory gcDataSetUploaderFactory;
-    private TopDataSetUploaderFactory topDataSetUploaderFactory;
-
-    private SdngTimeParserFactory sdngTimeParserFactory;
-    private GCTimeParserFactory gcTimeParserFactory;
-    private TopTimeParserFactory topTimeParserFactory;
+    private Map<String, Mode> modes;
 
     @Autowired
-    public LogParser(SdngDataParser sdngDataParser,
-                     GCDataParser gcDataParser,
-                     TopDataParser topDataParser,
-                     SdngDataSetUploaderFactory sdngDataSetUploaderFactory,
-                     GCDataSetUploaderFactory gcDataSetUploaderFactory,
-                     TopDataSetUploaderFactory topDataSetUploaderFactory,
-                     SdngTimeParserFactory sdngTimeParserFactory,
-                     GCTimeParserFactory gcTimeParserFactory,
-                     TopTimeParserFactory topTimeParserFactory){
-        this.sdngDataParser = sdngDataParser;
-        this.gcDataParser = gcDataParser;
-        this.topDataParser = topDataParser;
-
-        this.sdngDataSetUploaderFactory = sdngDataSetUploaderFactory;
-        this.gcDataSetUploaderFactory = gcDataSetUploaderFactory;
-        this.topDataSetUploaderFactory = topDataSetUploaderFactory;
-
-        this.sdngTimeParserFactory = sdngTimeParserFactory;
-        this.gcTimeParserFactory = gcTimeParserFactory;
-        this.topTimeParserFactory = topTimeParserFactory;
+    public LogParser(Map<String, Mode> modes){
+        this.modes = modes;
     }
 
     public void parse(String influxDb,
@@ -71,36 +39,16 @@ public class LogParser
                 System.getProperty("influx.password"),
                 requiredLogTrace);
 
-        DataParser dataParser;
-        TimeParserFactory timeParserFactory;
-        DataSetUploaderFactory uploaderFactory;
-
-        switch (mode)
-        {
-            case "sdng":
-                //Parse sdng
-                dataParser = sdngDataParser;
-                timeParserFactory = sdngTimeParserFactory;
-                uploaderFactory = sdngDataSetUploaderFactory;
-                break;
-            case "gc":
-                //Parse gc log
-                dataParser = gcDataParser;
-                timeParserFactory = gcTimeParserFactory;
-                uploaderFactory = gcDataSetUploaderFactory;
-                break;
-            case "top":
-                dataParser = topDataParser;
-                timeParserFactory = topTimeParserFactory;
-                uploaderFactory = topDataSetUploaderFactory;
-                break;
-            default:
-                throw new IllegalArgumentException(
-                        "Unknown parse mode! Availiable modes: sdng, gc, top. Requested mode: " + mode);
+        Mode parsingMode = modes.get(mode.toLowerCase());
+        if (parsingMode == null){
+            throw new IllegalArgumentException(
+                    "Unknown parse mode! Availiable modes: sdng, gc, top. Requested mode: " + mode);
         }
 
-        DataSetUploader dataSetUploader = uploaderFactory.create(uploaderParams);
-        TimeParser timeParser = timeParserFactory.create();
+        DataParser dataParser = parsingMode.getDataParser();
+        TimeParser timeParser = parsingMode.getTimeParser();
+        DataSetUploader dataSetUploader = parsingMode.getDataSetUploader(uploaderParams);
+
         timeParser.setLogPath(logPath);
 
         if (timeZone != null) {
